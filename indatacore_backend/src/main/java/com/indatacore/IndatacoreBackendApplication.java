0fd7e5a5d4bce8bf34d988ef6fc85346b99f21e1
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-05-28 14:38:47 +0100

    add environment backend

---
+++ indatacore_backend/src/main/java/com/indatacore/IndatacoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.indatacore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndatacoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndatacoreBackendApplication.class, args);
+	}
+
+}
