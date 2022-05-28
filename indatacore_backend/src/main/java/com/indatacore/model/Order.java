2e8de8f2b1f67c5a2f612acedb6abc795fb051f4
othmane-boudiab <mrboudiabothmane@gmail.com>
2022-05-28 14:56:15 +0100

    create model order and config application properties

---
+++ indatacore_backend/src/main/java/com/indatacore/model/Order.java
@@ -0,0 +1,22 @@
+package com.indatacore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "quantity")
+    private int quantity;
+}
